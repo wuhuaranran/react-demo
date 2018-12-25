9d266fe8c9a112342161d512bd1a64409de89776
xue.zhang <xue.zhang@weimob.com>
2018-12-25 15:53:52 +0800

    日历展示结果

--- src/pages/Ebbinghaus/index.js
+++ src/pages/Ebbinghaus/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
 import style from './index.less';
-import { Form, Layout, Input, Button, Timeline, Icon, Table } from 'antd';
+import { Form, Layout, Input, Button, Timeline, Icon, Table, Calendar, Badge, DatePicker } from 'antd';
+import { format } from 'path';
+import moment from 'moment';
 
 const FormItem = Form.Item;
 const {Header, Footer, Sider, Content} = Layout;
@@ -39,6 +41,7 @@ class Ebbinghaus extends React.Component {
             }
         });
     }
+    /* 计算任务计划 */
     calculate = (length, taskList) => {
         let result = [];
         let dataSource = [];
@@ -47,17 +50,19 @@ class Ebbinghaus extends React.Component {
             dataIndex: 'name',
             key: 'name'
         }];
+        /* 生成columns */
         for (let i = 0; i < Number(length) + 15; i++){
             result.push([]);
             columns.push({
                 title: i + 1,
                 dataIndex: i,
-                key: i,
+                key: 'c'+i,
                 render: (text, record, index) => (
                     text === 1 ? <Icon type="star" theme="twoTone" />:null
                 )
             })
         }
+        /* 生成result数组 用于渲染timeline */
         taskList.map((task,index)=>{
             result[index + 0].push(task); // 开始
             result[index + 1].push(task); // 1天
@@ -65,6 +70,8 @@ class Ebbinghaus extends React.Component {
             result[index + 4].push(task); // 4天
             result[index + 7].push(task); // 7天
             result[index + 15].push(task); // 15天
+
+            /* 生成dataSource 用于渲染表格 */
             dataSource[index]={
                 key: '任务' + (index + 1),
                 name: '任务' + (index + 1)
@@ -82,8 +89,50 @@ class Ebbinghaus extends React.Component {
             dataSource
         })
     }
+    /* 根据日期定义待办事项数组 */
+    getListData = (value) => {
+        let listData = [];
+        let startDate = new Date(this.state.startDate);
+        let thisData = new Date(value.format('YYYY-MM-DD'));
+        let currentDate;
+        if(startDate<=thisData){
+            currentDate = (thisData - startDate)/(1000*3600*24);
+        }
+        this.state.result.map((r,i)=>{
+            if (currentDate === i){
+                r.map(item=>{
+                    listData.push({
+                        type: 'success', 
+                        content: item
+                    })
+                })
+            }
+        })
+        return listData || [];
+    }
+    /* 渲染待办事项列表 */
+    dateCellRender=(value)=> {
+        const listData = this.getListData(value);
+        return (
+            <ul className="events">
+                {
+                    listData.map(item => (
+                        <li key={item.content}>
+                            <Badge status={item.type} text={item.content} />
+                        </li>
+                    ))
+                }
+            </ul>
+        );
+    }
+    /* 日期选择 */
+    startDateOnChange = (date, dateString)=>{
+        this.setState({
+            startDate: dateString
+        })
+    }
     render() {
-        const { result, columns, dataSource } = this.state;
+        const { result, columns, dataSource, startDate } = this.state;
         const { getFieldDecorator } = this.props.form;
         return (
             <div className={style.content}>
@@ -112,6 +161,16 @@ class Ebbinghaus extends React.Component {
                                         <Input />
                                     )}
                                 </FormItem>
+                                <FormItem
+                                    label="开始日期"
+                                    labelCol={{ span: 5 }}
+                                    wrapperCol={{ span: 15 }}
+                                    extra="请输入任务计划开始的日期"
+                                >
+                                    {getFieldDecorator('startDate')(
+                                        <DatePicker onChange={this.startDateOnChange}/>
+                                    )}
+                                </FormItem>
                                 <FormItem {...this.tailFormItemLayout}>
                                     <Button type="primary" htmlType="submit" className="login-form-button">计算</Button>
                                 </FormItem>
@@ -119,10 +178,13 @@ class Ebbinghaus extends React.Component {
                         </Sider>
                         <Content>
                             <h1>结果：</h1>
-                            <Timeline>
+                            <Timeline style={{ display: 'block' }}>
                                 {result.map((item, index) => {
                                     item = item.join('，');
-                                    return <Timeline.Item key={index}>第{index + 1}天：{item}</Timeline.Item>
+                                    let resultDate = new Date(startDate);
+                                    resultDate.setDate(resultDate.getDate() + index);
+                                    let displayDate = moment(resultDate).format('YYYY-MM-DD');
+                                    return <Timeline.Item key={index}>第{index + 1}天，{displayDate}：{item}</Timeline.Item>
                                 })}
                             </Timeline>
                         </Content>
@@ -132,6 +194,10 @@ class Ebbinghaus extends React.Component {
                     columns={columns} 
                     dataSource={dataSource} 
                     pagination={false}
+                />
+                <Calendar 
+                    dateCellRender={this.dateCellRender} 
+                    // monthCellRender={this.monthCellRender} 
                 />
             </div>
         )
